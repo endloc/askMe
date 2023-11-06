6df03b05785904543a4d5def07b765379123159c
endloc <li.amirova.l@gmail.com>
2023-11-07 02:51:08 +0300

    make layout extend and question-item component

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# request - our HTTP-request
+def index(request):
+    questions = [
+        {
+            'id': i,
+            'title': f'How to build a moon park {i}',
+            'content': f'Long Lorem ipsum {i}'
+        } for i in range(10)
+    ]
+    return render(request, 'index.html', {'questions': questions})
+
+
+def question(request):
+    return render(request, 'question.html')
