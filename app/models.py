8eb29722085fa1e734bc3c3af3d79be782fb4e66
endloc <li.amirova.l@gmail.com>
2023-12-04 17:59:40 +0300

    str methods added

--- app/models.py
+++ app/models.py
@@ -6,23 +6,36 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     nickname = models.CharField(max_length=100)
     avatar = models.ImageField(max_length=255, blank=True)
-    
+
 
 class QuestionManager(models.Manager):
-    def new(self):
+    def new_questions(self):
         return super().get_query_set().order_by('-date')
 
-    def hot(self):
+    def hot_questions(self):
         return super().get_query_set().order_by('-likes_count')
 
-    def by_tag(self, tag):
-        return super().get_query_set().filter(tags__name=tag)
+    def questions_by_tag(self, tag):
+        return super().get_query_set().filter(tags__name=tag).order_by('-date')
+
+
+class AnswerManager(models.Manager):
+    def answers_by_question(self, required_question):
+        # try:
+        return self.all().filter(question=required_question).order_by('-date')
+        # except self.model.DoesNotExist as e:
+        #     return Answer.objects.none()
+
+
+class TagManager(models.Manager):
+    def tag_by_title(self, title):
+        return self.filter(tag_title__exact=title)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
 
-    objects = models.Manager()
+    objects = TagManager()
 
     def __str__(self):
         return self.name
@@ -32,8 +45,8 @@ class Question(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='questions')
-    tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, related_name="questions")
     date = models.DateTimeField(auto_now_add=True)
     # likes = models.ManyToManyField(User, through='QuestionLike', related_name='liked_questions')
     likes_count = models.IntegerField(default=0)
@@ -42,7 +55,7 @@ class Question(models.Model):
     objects = QuestionManager()
 
     def __str__(self):
-        return self.title
+        return f"{self.id} {str(self.title)}"
 
     # class Meta:
     #     verbose_name = 'question'
@@ -61,21 +74,24 @@ class QuestionLike(models.Model):
     class Meta:
         unique_together = ('user', 'question')
 
+    def __str__(self):
+        return f"to question: {self.question}; from user: {self.user}"
+
 
 class Answer(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='answers')
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     text = models.TextField()
     status = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
     # likes = models.ManyToManyField(User, through='AnswerLike', related_name='liked_answers')
     likes_count = models.IntegerField(default=0)
 
-    objects = models.Manager()
+    objects = models.AnswerManager()
 
     def __str__(self):
-        return self.text[:min(70, len(self.text))] + "..."
+        return f"to question: {self.question}; answer text: {self.text[:min(40, len(str(self.text)))]} ..."
 
     # class Meta:
     #     verbose_name = 'answer'
@@ -88,3 +104,6 @@ class AnswerLike(models.Model):
 
     class Meta:
         unique_together = ('user', 'answer')
+
+    def __str__(self):
+        return f"to answer: {self.answer}; from user: {self.user}"
